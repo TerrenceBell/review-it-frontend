83f16d6120ed565195d36460505a536fe2a726e7
TerrenceBell <terrencekbell@gmail.com>
2020-06-28 16:38:27 -0500

    adding code for item class

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ fetch("http://localhost:3000/items")
 .then(resp => resp.json())
 .then(items => {
   items.forEach(item => {
-    
+    const { id, name, category, description} = item 
+    new Item(id, name, category, description)
   });
 })
+// have form to create item... maybe
+// have form to create review
 
 console.log("testing...")
 
--- src/item.js
+++ src/item.js
@@ -1,3 +1,14 @@
 class Item {
-    
+
+    constructor(id,name,category,description){
+        this.id = id 
+        this.name = name
+        this.category = category
+        this.description = description
+    }
+
+//render all items
+// delete items?
+//link items to show page
+
 }
